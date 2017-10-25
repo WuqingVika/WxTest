7210129a43fb5c5b4be0c24334c9247236222a89
wuqingvika <526288022@qq.com>
2017-10-25 22:38:25 +0800

    test

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,1 +1,15 @@
-Page({});
+Page({
+    onTap:function(){
+        //console.log("ontap"); 别加.wxml
+        //ctrl+i 选中当前行
+        //  wx.navigateTo({url:"../posts/posts"}); //从父级跳到子级页面的关系
+        //注意：子级最多只有有5级
+         wx.redirectTo({url:"../posts/posts"});
+    },
+    onUnload:function(){
+        console.log("w redirectTo onUnload ");
+    },
+    onHide:function(){
+        console.log("w navigateTO onHide");
+    }
+});
