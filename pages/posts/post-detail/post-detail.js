f0289e70c74287e0e89a46799faa6387c44d9cd7
wuqingvika <526288022@qq.com>
2017-11-11 14:31:27 +0800

    6-5缓存Storage的基本用法

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -7,5 +7,16 @@ Page({
         var postData=postsData.postList[postId];
         //console.log(postData);
         this.setData( {postData:postData});
+    },
+    onCollection:function(event){
+        // wx.setStorageSync('key', "wuqingvika");
+        wx.setStorageSync('key', {
+            name:"wuqingvika",
+            age:23
+        })
+    },
+    onShare:function(event){
+        //wx.clearStorageSync();//清空所有缓存
+        wx.removeStorageSync('key');//移除key所对应的缓存
     }
 })
