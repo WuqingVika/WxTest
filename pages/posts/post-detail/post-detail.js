4999e57368baad6da09f3e8e81ab34906c099e39
wuqingvika <526288022@qq.com>
2017-11-11 16:43:55 +0800

    6-6 使用缓存实现文章收藏功能

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,22 +1,49 @@
-var postsData=require('../../../data/posts-data.js');
+var postsData = require('../../../data/posts-data.js');
 
 Page({
-    onLoad:function(option){
-        var postId=option.id;
+    onLoad: function (option) {
+        var postId = option.id;
         //console.log(id);
-        var postData=postsData.postList[postId];
+        var postData = postsData.postList[postId];
         //console.log(postData);
-        this.setData( {postData:postData});
+        // this.setData( {postData:postData});
+        this.setData({ postData: postData, postCurrentId: postId });
+        //获取缓存
+        var postsCollected = wx.getStorageSync('posts_collected');
+        if (postsCollected) {
+            //有缓存获取当前文章的缓存
+            var postCollected = postsCollected[postId];
+            //绑定到页面
+            this.setData({
+                collected: postCollected
+            })
+        } else {
+            //没有,初始化缓存列表
+            var postsCollectedNothing = {};
+            postsCollectedNothing[postId] = false;//将当前状态置为false,再塞到缓存列表中
+            wx.setStorageSync('posts_collected', postsCollectedNothing);
+        }
     },
-    onCollection:function(event){
+    onCollection: function (event) {
         // wx.setStorageSync('key', "wuqingvika");
-        wx.setStorageSync('key', {
-            name:"wuqingvika",
-            age:23
-        })
+        // wx.setStorageSync('key', {
+        //     name:"wuqingvika",
+        //     age:23
+        // })
+
+        //1.先获取缓存中当前文章的状态
+        var postsCollected = wx.getStorageSync('posts_collected');
+        var postCollected = postsCollected[this.data.postCurrentId];
+        //取到当前文章的状态，取反
+        postCollected = !postCollected;
+        //将新的状态塞进缓存修改
+ postsCollected[this.data.postCurrentId]=postCollected;
+        wx.setStorageSync('posts_collected', postsCollected);
+        //更新页面显示状态
+        this.setData({collected:postCollected});
     },
-    onShare:function(event){
-        //wx.clearStorageSync();//清空所有缓存
-        wx.removeStorageSync('key');//移除key所对应的缓存
-    }
+    // onShare:function(event){
+    //     //wx.clearStorageSync();//清空所有缓存
+    //     wx.removeStorageSync('key');//移除key所对应的缓存
+    // }
 })
